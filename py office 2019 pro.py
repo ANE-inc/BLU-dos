329805feb20b9e9e6c63ac4463501cd75b60855d
ANE inc <49503606+ANEinc-githubpush@users.noreply.github.com>
2019-05-12 12:26:19 -0700

    Add files via upload

---
+++ py office 2019 pro.py
@@ -0,0 +1,107 @@
+# ANE python enviroment v 2.5 for 2019 |pro|
+
+# To use enviroment,tap run, and save.
+# you can only add and subtract 1 digit numbers.
+
+print ('Welcome to...')
+print('ANE python environment v 2.5 for 2019 |pro|!')
+print()
+print('Note: you can only add and subtract 1 digit numbers.')
+print()
+print('Commands:')
+print('toolcountbytwo (x)')
+print()
+print('tooladd (a,b)')
+print()
+print('toolsubtract (c,d)')
+print()
+print('toolbox ()')
+print()
+print('toolcountupto (n)')
+print()
+print('toolmakerandomnumber(i,x)')
+print()
+print('===============================')
+print ('Type helpyman() to show this again')
+
+
+def toolcountupto (n):
+    m = 0
+    while (m<n):
+        print (m)
+        m = m + 1
+
+def toolcountbytwo (x):
+    y = 0
+    while (y<x):
+        print (y)
+        y = y + 2
+
+def tooladd (a,b):
+    print ( a + b )
+
+def toolsubtract (c,d):
+    print ( c - d )
+
+def toolbox ():
+    print(' tools are toolcountupto () toolcountbytwo () tooladd ()toolsubtract () toolbox() toolmakerandomnumber(i,x), and helpyman ()')
+
+
+def toolMakeRandomNumber(i,x):
+    m = 0
+    while (m<i):
+        m = m + x
+
+    m = m + i
+    m = m + i
+    m = m + i
+    m = m + i
+    m = m + i
+
+    f = 0
+
+    f = m + m
+    f = f + m + i
+
+    print (f)
+
+def helpyman ():
+     print('Help for ANE python environment v 2.5')
+     print ('# To use environment, tap run, and save.')
+     print('# you can only add and subtract 1 digit numbers.')
+     print('Commands:')
+     print('toolcountbytwo (x)')
+     print()
+     print('tooladd (a,b)')
+     print()
+     print('toolsubtract (c,d)')
+     print()
+     print('toolbox ()**')
+     print()
+     print('toolcountupto (n)')
+     print()
+     print('toolmakerandomnumber(i,x)*')
+     print()
+def toolpickrandomletter():
+    print ('tool is not stable!')
+    #toolmakerandomnumber(3,4)
+
+    #o = f
+    #while (o>26):
+     #   o/2
+
+
+    #if (o = 14 + 1):
+     #   print (o)
+
+
+def build ():
+    print ('1272098117728262019')
+    print ('vershion 2.5')
+    li = 730262019
+    print ('Your key is:')
+    print (li)
+
+def pycalc ():
+    print ('If you have the py ware pack, your good to go, run the py calc module,otherwise you can down load it here :')
+    print('https://drive.google.com/file/d/15sEVVyV6Md-9S6YnoHBM_QAt8TQ7Ok46/view')
